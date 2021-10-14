67dabbe952ab3ee919dc724d4640d2c5c74ad036
Fernando Rocha <fhrlobacz@gmail.com>
2021-10-14 11:11:47 -0300

    testando o flask

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from selenium import webdriver
+from flask import Flask
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+app = Flask(__name__)
 
-driver.get('https://www.google.com')
-pesquisa = driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/div[1]/a[4]').text
-print(pesquisa)
-driver.quit()
+def abrir_navegador():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    return driver
+
+def realizar_teste(driver):
+    driver.get('https://www.google.com')
+    pesquisa = driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/div[1]/a[4]').text
+    print(pesquisa)
+    driver.quit()
+
+@app.route('/')
+def index():
+    return 'Ok!'
+
+@app.route('/teste')
+def teste():
+    return 'Tentativa'
+
+if __name__ == '__main__':
+    app.run()

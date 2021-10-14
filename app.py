edc8925065ba3beea389b868c484e607d8d3d010
Fernando Rocha <fhrlobacz@gmail.com>
2021-10-14 14:00:04 -0300

    fix async

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask.json import jsonify
 from selenium import webdriver
 from flask import Flask, jsonify
-import asyncio
 import os
+import asyncio
 
 app = Flask(__name__)
 
 async def abrir_navegador():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.get('https://www.google.com')
     pesquisa = driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/div[1]/a[4]').text
     driver.quit()
@@ -23,7 +21,7 @@ def index():
     return 'Ok!'
 
 @app.route('/teste')
-async def teste():
+def teste():
     asyncio.set_event_loop(asyncio.new_event_loop())
     loop = asyncio.get_event_loop()
     result = loop.run_until_complete(abrir_navegador())

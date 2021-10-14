e2c656b18bf16d8189bdeca6546b8844a0f06fcd
Fernando Rocha <fhrlobacz@gmail.com>
2021-10-14 14:09:16 -0300

    reposto binary_location

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 async def abrir_navegador():
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    #driver = webdriver.Chrome(executable_path=("chromedriver.exe"), options=chrome_options)
     driver.get('https://www.google.com')
     pesquisa = driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/div[1]/a[4]').text
     driver.quit()

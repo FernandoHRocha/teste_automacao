173c2d473a9cc082dcbc2453895975be661f4fc3
Fernando Rocha <fhrlobacz@gmail.com>
2021-10-14 10:44:00 -0300

    fix

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 driver.get('https://www.google.com')
-pesquisa = driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/div[1]/a[4]')
+pesquisa = driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/div[1]/a[4]').text
 print(pesquisa)
+driver.quit()
